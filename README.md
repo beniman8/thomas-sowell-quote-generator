33514a057bd6624548a3c9545e772a870c47def9
Beni <benik@live.ca>
2022-09-27 22:55:01 -0400

    Finish the quote generator using csv

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # thomas-sowell-quote-generator
 This is a small python program that generates random quotes from thomas sowell one of my favorite writers
+
+Some quotes are in spanish we have about 876 quotes !!!!
---
+++ linecount.py
@@ -0,0 +1,22 @@
+import csv
+import random
+
+# file = open("quotes.jl","r",encoding="utf8")
+# line_count = 0
+
+
+# for line in file:
+#     if line !="\n":
+#         line_count += 1
+
+# file.close()
+
+# print(line_count)
+
+
+
+with open("quotes.csv", "r",encoding="utf8") as t_s_quotes:
+    csv_reader = csv.reader(t_s_quotes)
+    # This skips over the first line in the .csv (author, text)
+    next(csv_reader)
+    print(random.choice([f"{line[1]} ({line[0]})" for line in csv_reader]))
